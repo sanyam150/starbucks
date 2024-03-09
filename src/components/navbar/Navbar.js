6f523efb222156e6b962cb990b65c384a6ccaefd
sanyam150 <sanyamvirmani1992@gmail.com>
2024-03-09 16:43:05 +0530

    Create Navbar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import "./navbar.css";
+import { FaLocationDot } from "react-icons/fa6";
+import { colors } from "../../globalStyle";
+import ActionButton from "../actionButton/ActionButton";
 
 const Navbar = () => {
+  const navbarList = ["MENU", "REWARDS", "GIFTS"];
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar_container">
+      <div className="navbar_container_wrapper">
+        <div id="navbar_logo_list_wrapper">
+          <div id="navbar_logo_container">
+            <img
+              src="/starbucks.png"
+              alt="starBucks_logo"
+              id="starbucks_logo"
+            />
+          </div>
+          <div id="navbar_list_container">
+            <ul id="list_style">
+              {navbarList.map((listItem, index) => {
+                return (
+                  <li className="list_style_items" key={index}>
+                    {listItem}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+        <div id="navbar_useroptions_container">
+          <div id="location_container" className="user_options_navbar">
+            <div
+              style={{
+                fontSize: "22px",
+                display: "flex",
+                alignItems: "center",
+                margin: "0 5px",
+              }}
+            >
+              <FaLocationDot />
+            </div>
+            <span
+              style={{
+                fontSize: "17px",
+                display: "flex",
+                alignItems: "center",
+                fontFamily: "system-ui",
+              }}
+            >
+              Find a Store
+            </span>
+          </div>
+          <div className="user_options_navbar">
+            <ActionButton
+              buttonColor={colors.colorBlack}
+              buttonText={"join now"}
+            />
+          </div>
+          <div className="user_options_navbar">
+            <ActionButton
+              buttonColor={colors.colorBlack}
+              buttonText={"sign up"}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
